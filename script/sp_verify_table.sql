9e3b989bea17b55c06fd5a6639168ec606b26587
passakorn`B`choosuk <passakorn.choosuk@itone.co.th>
2021-09-07 18:50:48 +0700

    Add compare data type, pk, unique

--- script/sp_verify_table.sql
+++ script/sp_verify_table.sql
@@ -6,7 +6,7 @@ BEGIN
     -- compare column
     SELECT m.a_table_name                                  AS 'design_table_name',
            m.a_column_name                                 AS 'design_column_name',
-           c.column_name                                   AS 'impl_table_name',
+           c.column_name                                   AS 'impl_column_name',
            IF(m.a_column_name = c.column_name, '', 'DIFF') AS 'compare_result'
     FROM mfoa_table_spec m
              LEFT JOIN information_schema.columns c
@@ -15,6 +15,107 @@ BEGIN
     WHERE m.a_table_name = p_table_name
       AND c.table_schema = p_schema_name
     ORDER BY compare_result DESC, m.a_column_name;
+    -- compare column
+
+    -- compare data type name
+    SELECT m.a_table_name                            AS 'design_table_name',
+           m.a_column_name                           AS 'design_column_name',
+           m.data_type                               AS 'design_data_type',
+           c.data_type                               AS 'impl_data_type',
+           IF(m.data_type = c.data_type, '', 'DIFF') AS 'compare_result'
+    FROM mfoa_table_spec m
+             LEFT JOIN information_schema.columns c
+                       ON m.a_column_name = c.column_name
+                           AND m.a_table_name = c.table_name
+    WHERE m.a_table_name = p_table_name
+      AND c.table_schema = p_schema_name
+    ORDER BY compare_result DESC, m.a_column_name;
+    -- compare data type name
+
+    -- compare nullable
+    SELECT m.a_table_name                                                            AS 'design_table_name',
+           m.a_column_name                                                           AS 'design_column_name',
+           m.is_nullable                                                             AS 'design_is_nullable',
+           c.is_nullable                                                             AS 'impl_is_nullable',
+           IF(m.is_nullable = IF(c.is_nullable LIKE 'YES', TRUE, FALSE), '', 'DIFF') AS 'compare_result'
+    FROM mfoa_table_spec m
+             LEFT JOIN information_schema.columns c
+                       ON m.a_column_name = c.column_name
+                           AND m.a_table_name = c.table_name
+    WHERE m.a_table_name = p_table_name
+      AND c.table_schema = p_schema_name
+    ORDER BY compare_result DESC, m.a_column_name;
+    -- compare nullable
+
+    -- compare PK
+    SELECT m.a_table_name                        AS 'design_table_name',
+           m.a_column_name                       AS 'design_column_name',
+
+           IF(m.is_pk = TRUE, 'PK', '')          AS 'design_pk',
+           IF(temp1.is_pk = TRUE, 'PK', '')      AS 'impl_pk',
+           IF(m.is_pk = temp1.is_pk, '', 'DIFF') AS 'compare_result'
+    FROM mfoa_table_spec m
+             LEFT JOIN (SELECT DISTINCT c.table_name,
+                                        c.column_name,
+                                        IF(temp1.constraint_type = 'PRIMARY KEY', TRUE, FALSE) AS 'is_pk'
+                        FROM information_schema.columns c
+                                 LEFT JOIN (SELECT DISTINCT s.table_schema,
+                                                            s.table_name,
+                                                            tc.constraint_type,
+                                                            s.column_name
+                                            FROM information_schema.statistics s
+                                                     INNER JOIN information_schema.table_constraints tc
+                                                                ON s.table_schema = tc.table_schema AND s.table_name = tc.table_name
+                                            WHERE s.table_schema = p_schema_name
+                                              AND s.table_name = p_table_name
+                                              AND tc.constraint_type = 'PRIMARY KEY') AS temp1
+                                           ON c.table_schema = temp1.table_schema AND
+                                              c.table_name = temp1.table_name AND
+                                              c.column_name = temp1.column_name
+                        WHERE c.table_schema = p_schema_name
+                          AND c.table_name = p_table_name
+                          AND c.column_name NOT IN
+                              ('updated_by', 'updated_date_time', 'created_by', 'created_date_time')) AS temp1
+                       ON m.a_table_name = temp1.table_name AND m.a_column_name = temp1.column_name
+    WHERE m.a_table_name = p_table_name
+    ORDER BY compare_result, design_pk DESC;
+    -- compare PK
+
+    -- compare UNIQUE
+    SELECT m.a_table_name                                AS 'design_table_name',
+           m.a_column_name                               AS 'design_column_name',
+
+           IF(m.is_unique = TRUE, 'UNIQUE', '')          AS 'design_unique',
+           IF(temp1.is_unique = TRUE, 'UNIQUE', '')      AS 'impl_unique',
+           IF(m.is_unique = temp1.is_unique, '', 'DIFF') AS 'compare_result'
+    FROM mfoa_table_spec m
+             LEFT JOIN (SELECT DISTINCT c.table_name,
+                                        c.column_name,
+                                        IF(temp1.constraint_type = 'UNIQUE', TRUE, FALSE) AS 'is_unique'
+                        FROM information_schema.columns c
+                                 LEFT JOIN (SELECT DISTINCT s.table_schema,
+                                                            s.table_name,
+                                                            tc.constraint_type,
+                                                            s.column_name
+                                            FROM information_schema.statistics s
+                                                     INNER JOIN information_schema.table_constraints tc
+                                                                ON s.table_schema = tc.table_schema AND s.table_name = tc.table_name
+                                            WHERE s.table_schema = p_schema_name
+                                              AND s.table_name = p_table_name
+                                              AND tc.constraint_type = 'UNIQUE') AS temp1
+                                           ON c.table_schema = temp1.table_schema AND
+                                              c.table_name = temp1.table_name AND
+                                              c.column_name = temp1.column_name
+                        WHERE c.table_schema = p_schema_name
+                          AND c.table_name = p_table_name
+                          AND c.column_name NOT IN
+                              ('updated_by', 'updated_date_time', 'created_by', 'created_date_time')) AS temp1
+                       ON m.a_table_name = temp1.table_name AND m.a_column_name = temp1.column_name
+    WHERE m.a_table_name = p_table_name
+    ORDER BY compare_result DESC, design_unique DESC;
+    -- compare UNIQUE
+
+
 END //
 
 DELIMITER ;
